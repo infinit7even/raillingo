725938c8805e93aeddf385a091d0d940664d57b6
infinit7even <infinit7even@gmail.com>
2026-08-03 10:23:57 +0200

    refactor: pulizia codice ed eliminazione di rotte e file inutilizzati o deprecati

--- src/routes/api/auth/[...all]/+server.ts
+++
@@ -1,5 +0,0 @@
-import { auth } from '$lib/auth';
-import type { RequestHandler } from '@sveltejs/kit';
-
-export const GET: RequestHandler = ({ request }) => auth.handler(request);
-export const POST: RequestHandler = ({ request }) => auth.handler(request);
--- src/routes/api/users/+server.ts
+++
@@ -1,9 +0,0 @@
-import { json, type RequestHandler } from '@sveltejs/kit';
-
-export const GET: RequestHandler = async () => {
-	return json({ users: [] });
-};
-
-export const POST: RequestHandler = async () => {
-	return json({ error: 'Gestione utenti disattivata. Autenticazione riservata unicamente all\'amministratore.' }, { status: 400 });
-};
